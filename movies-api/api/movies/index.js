8d675f63a06fcb95933e0d9a38dee8fbc8a5f271
Qi Zeng <ivan_zq@foxmail.com>
2022-01-05 17:46:05 +0000

    CA - Adding Get Reviews & Post Reviews Router

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,7 +3,14 @@ import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid'
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
-import {getUpcomingMovies, getNowPlayingMovies, getTopRatedMovies, getPopularMovies, getSimilarMovies, getMovies, getMovie, getMovieImages} from '../tmdb-api';
+import {getUpcomingMovies, 
+        getNowPlayingMovies, 
+        getTopRatedMovies, 
+        getPopularMovies, 
+        getSimilarMovies, 
+        getMovies, getMovie, 
+        getMovieImages,
+        getMovieReviews} from '../tmdb-api';
 
 
 const router = express.Router(); 
@@ -51,10 +58,11 @@ router.get('/:id/images', asyncHandler(async (req, res) => {
 }));
 
 // Get movie reviews
-router.get('/:id/reviews', (req, res) => {
+router.get('/:id/reviews', asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
+    const movieReviews = await getMovieReviews(id);
     // find reviews in list
-    if (movieReviews.id == id) {
+    if (movieReviews) {
         res.status(200).json(movieReviews);
     } else {
         res.status(404).json({
@@ -62,16 +70,16 @@ router.get('/:id/reviews', (req, res) => {
             status_code: 404
         });
     }
-});
+}));
 
-router.post('/:id/reviews', (req, res) => {
+router.post('/:id/reviews', asyncHandler(async (req, res) => {
     const id = parseInt(req.params.id);
-
-    if (movieReviews.id == id) {
+    const reviews = await getMovieReviews(id);
+    if (reviews.id == id) {
         req.body.created_at = new Date();
         req.body.updated_at = new Date();
         req.body.id = uniqid();
-        movieReviews.results.push(req.body); //push the new review onto the list
+        reviews.results.push(req.body); //push the new review onto the list
         res.status(201).json(req.body);
     } else {
         res.status(404).json({
@@ -79,7 +87,7 @@ router.post('/:id/reviews', (req, res) => {
             status_code: 404
         });
     }
-});
+}));
 
 router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
     const upcomingMovies = await getUpcomingMovies();
